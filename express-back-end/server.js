53b1fb7726f0eaec6ff6ea2efe5d3ba6300def97
Divya Jagadish <divyajagadish21@gmail.com>
2021-05-09 18:15:55 +0000

    checking routes

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -1,19 +1,44 @@
 const Express = require("express");
+require("dotenv").config();
 const App = Express();
 const BodyParser = require("body-parser");
 const PORT = 8080;
+const fs = require("fs");
+const chalk = require("chalk");
+const pg = require("pg");
 
+// PG connection setup
+let result;
+var conString = process.env.POSTGRES_URL;
+//Can be found in the Details page
+var client = new pg.Client(conString);
+client.connect(function (err) {
+	if (err) {
+		return console.error("could not connect to postgres", err);
+	}
+});
 // Express Configuration
 App.use(BodyParser.urlencoded({ extended: false }));
 App.use(BodyParser.json());
 App.use(Express.static("public"));
+const gettasksWithCategory = function () {
+	return client
+		.query(`SELECT name  FROM USERS where id = 1`)
+		.then((res) => {
+			return res.rows[0];
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+};
 
 // Sample GET route
-App.get("/api/data", (req, res) =>
-	res.json({
-		message: "Seems to work!",
-	})
-);
+App.get("/api/data", (req, res) => {
+	gettasksWithCategory().then((result) => {
+		console.log(result);
+		res.json({ message: result["name"] });
+	});
+});
 
 App.listen(PORT, () => {
 	// eslint-disable-next-line no-console
