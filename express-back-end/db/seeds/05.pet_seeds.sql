649ee1dba2be0defa421ca5c055b12ad48bb29d6
Syransong <ksysong@gmail.com>
2021-05-08 17:34:12 -0700

    create pets seeds data

---
+++ express-back-end/db/seeds/05.pet_seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO pet_shop (name, description, required_challenge_count, img, coins)
+VALUES ("Mango", "A little empty-headed but will always be loyal to you", 0, "docs/pets/051-cat.png", 0),
+("Oreo", "Loves knitting cozy sweaters. Wants to try embroidery next", 2, "docs/pets/051-siberian-husky.png", 100),
+("Reef", "Loves surfing. Favourite tricks: Alley-Oop and the Rodeo Flip", 4, "docs/pets/051-turtle.png", 200),
+("Penelope", "The secret to her flawless skin is mud masks", 6, "docs/pets/051-pig.png", 400),
+("Marty", "Enjoys hot cocoa and anything plaid", 8, "docs/pets/051-moose.png", 400),
+("Eleanor", "Current hobby is painting still-lifes and portraits", 10, "docs/pets/051-elephant.png", 600),
+("Bao Bao", "Take Bao Bao with you to buffets to get your money's worth", 12, "docs/pets/051-panda.png", 800),
+("Theodore", "Will not share his mane routine no matter how many times you ask", 16, "docs/pets/051-lion.png", 1000);
+
+INSERT INTO my_pets (isActive, pet_id, user_id)
+VALUES (TRUE, 1, 1),
+(TRUE, 1, 2), 
+(TRUE, 1, 3);
