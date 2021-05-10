9cb3257c17b2927e1e8ca929931f82f72d220e45
Syransong <ksysong@gmail.com>
2021-05-09 23:09:16 -0700

    add prelim code to pet shop page and list item

--- react-front-end/src/components/PetShopListItem.js
+++ react-front-end/src/components/PetShopListItem.js
@@ -1,19 +1,37 @@
 import React from "react";
-import Card from "@material-ui/core/Card";
+// import Card from "@material-ui/core/Card";
 import Typography from "@material-ui/core/Typography";
-import { CardMedia } from "@material-ui/core";
+// import { CardMedia } from "@material-ui/core";
+import Button from "@material-ui/core/Button";
+import MonetizationOnRoundedIcon from '@material-ui/icons/MonetizationOnRounded';
 
 export default function DigitalPet(props) {
 	console.log(props);
 	return (
-		<Card>
-			<CardMedia style={{ height: "150px" }} image={props.Activepet.img} />
-			<Typography>{props.Activepet.name}</Typography>
-		</Card>
+		// <Card>
+		// 	<CardMedia style={{ height: "150px" }} image={props.Activepet.img} />
+		// 	<Typography>{props.Activepet.name}</Typography>
+		// </Card>
+
+    <Grid item xs={6} sm={4}>
+      <img src={props.img}/>
+      <Typography variant="h2">
+        {props.name}
+      </Typography>
+      <Typography variant="subtitle1">
+        {props.description}
+      </Typography>
+      <Typography variant="subtitle1">
+        {props.coins}
+      </Typography>
+      <Button variant="contained">
+        <MonetizationOnRoundedIcon/>
+        BUY
+      </Button>
+    </Grid>
 	);
 }
 
 // Info wanted from Props 
 // Name, Description, Coins
 // need button for buy
-// need to show player coins 
---
+++ react-front-end/src/pages/PetShopPage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+// import { makeStyles } from '@material-ui/core/styles';
+import Typography from "@material-ui/core/Typography";
+import Grid from '@material-ui/core/Grid';
+import PetShopListItem from '../components/PetShopListItem ';
+import Nav from '../components/Nav';
+
+export default function PetShopPage(props) {
+  <>
+    <Nav/>
+    <Typography variant="h1">Pet Shop</Typography>
+    <Typography variant="subtitle2">
+      {/* how to get user coins  */}
+    </Typography>
+    <Grid container direction="column" spacing={2}>
+      {/* Map through each pet item, not sure how to do it quite yet */}
+    </Grid>
+  </>
+};
+
+//Want to also display user coins 
