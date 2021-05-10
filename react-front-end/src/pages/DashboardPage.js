50b287959d854d869b175b2ddf2486d1db9b4411
Divya Jagadish <divyajagadish21@gmail.com>
2021-05-10 16:52:23 +0000

    dashboard Page

--- react-front-end/src/pages/DashboardPage.js
+++ react-front-end/src/pages/DashboardPage.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
-
 import Nav from "../components/Nav";
 import DigitalPet from "../components/DigitalPet";
 import Status from "../components/status";
