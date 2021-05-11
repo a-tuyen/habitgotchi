0307b5493fedfe9009f1bd624cac976fb3727a7b
Syransong <ksysong@gmail.com>
2021-05-11 11:58:13 -0700

    fiddling with styling of Nav

--- react-front-end/src/components/Nav.js
+++ react-front-end/src/components/Nav.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { AppBar, Toolbar, Typography, Link} from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
+import PetsIcon from '@material-ui/icons/Pets';
 
 //Stylesheet
 import "./styles/Nav.scss";
@@ -9,7 +10,11 @@ export default function Nav() {
 	return (
 		<AppBar position="static" className="nav-bar">
 			<Toolbar className="nav-bar--container">
-				<Typography>HabitGotchi</Typography>
+        <span className="app-name">
+          {/* <PetsIcon/> */}
+          <h3>HabitGotchi</h3>
+          {/* <Typography variant="h3">HabitGotchi</Typography> */}
+        </span>
 				<MenuIcon />
 			</Toolbar>
 		</AppBar>
