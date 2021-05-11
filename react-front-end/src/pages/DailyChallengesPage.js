243c4d03482545af1b680a727cce6d8040b9f968
Amanda <atuyen26@gmail.com>
2021-05-11 16:14:19 -0700

    fix bug with dailychallenges page rendering

--- react-front-end/src/pages/DailyChallengesPage.js
+++ react-front-end/src/pages/DailyChallengesPage.js
@@ -9,13 +9,23 @@ export default function DailyChallengesPage() {
 	// console.log('DailyChallengeprops:', props.dailyChallenges);
 	// console.log('props.stepcount:', props.dailyChallenges.step_count);
   const dailyChallenges = useContext(ChallengeContext)
-  console.log('dailyChall:', dailyChallenges.DailyChallenges.step_goal)
+  console.log('dailyChall:', dailyChallenges.DailyChallenges)
+  const getChallenges = (data) => {
+    console.log('data:', data)
+    // console.log('data:', data.DailyChallenges.length)
+    if (data.DailyChallenges.length) {
+      console.log('datalengthinside:', data.DailyChallenges.length)
+      return data.DailyChallenges.map(item => {
+        return <div>{item.step_goal}</div>
+      })
+    }
+  }
   // const Arr = Object.entries(props.dailyChallenges)
 	return (
     <div>
       <Nav />
      {/* <h1>{dailyChallenges.step_goal}</h1> */}
-         
+        {getChallenges(dailyChallenges)}
     </div>
 
   );
