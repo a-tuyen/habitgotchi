d86de69df859b52e1afd8b6fb2e9b18da9980dee
Syransong <ksysong@gmail.com>
2021-05-12 22:26:27 -0700

    fiddled with my pet inventory styling

--- react-front-end/src/components/myPetInventoryListItem.js
+++ react-front-end/src/components/myPetInventoryListItem.js
@@ -5,18 +5,47 @@ import { CardMedia, GridListTileBar } from "@material-ui/core";
 import { Button } from "@material-ui/core";
 import { useContext } from "react";
 import SelectContext from "./SelectContext";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 
-
+const useStyles = makeStyles((theme) => ({
+	root: {
+		width: "100%",
+		maxWidth: "80%",
+		marginLeft: "5vw",
+		marginRight: "5vw",
+		marginTop: "5vh",
+		padding: "5em",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: "2em",
+    minHeight: "3vh",
+    // height: "18rem",
+    // width: '14rem',
+	},
+  img: {
+    height: "25vh",
+    padding: "2em"
+  },
+  button: {
+    backgroundColor: "#2B7A78",
+    fontFamily: "Quicksand"
+  }
+}));
 
 export default function MyPetInventoryListItem(props) {
+  const classes = useStyles();
 	// console.log(props);
 	const selectdigitalpet = useContext(SelectContext);
 	return (
-		<Card>
-			<img src={props.img} />
-			<h3>{props.name}</h3>
-			<p>{props.description}</p>
+		<Card className={classes.root}>
+      <div className="pet-content">
+        <img src={props.img} className={classes.img}/>
+        <h3>{props.name}</h3>
+        <p>{props.description}</p>
+      </div>
 			<Button
+        className={classes.button}
 				variant="contained"
 				color="Primary"
 				onClick={() => {
--- react-front-end/src/pages/InventoryPage.js
+++ react-front-end/src/pages/InventoryPage.js
@@ -3,12 +3,28 @@ import React from "react";
 import Grid from "@material-ui/core/Grid";
 import MyPetlist from "../components/MyPetList";
 import Nav from "../components/Nav";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		// width: "80%",
+		// maxWidth: "80%",
+		marginLeft: "2.5vw",
+		marginRight: "2.5vw",
+		// marginTop: "2vh",
+		// padding: "5em",
+	},
+}));
+
 export default function InventoryPage(props) {
+  const classes = useStyles();
 	return (
 		<div>
 			<Nav />
-			<h1>My Inventory</h1>
-			<MyPetlist myPetInventory={props.myPetInventory} />
+        <div className={classes.root}>
+          <h1>My Inventory</h1>
+          <MyPetlist myPetInventory={props.myPetInventory} className={classes.root} />
+        </div>
 		</div>
 	);
 }
